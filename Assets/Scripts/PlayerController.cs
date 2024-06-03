3472f91d9b792c5b72c164a92e2d2ba74193e6f0
dkvjdj <102583595+dkvjdj@users.noreply.github.com>
2024-06-03 20:23:08 +1100

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,6 +14,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime * speed);
+        if (Input.GetKey(KeyCode.W)) {
+            transform.Translate(new Vector3.forward * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            transform.Translate(new Vector3.back * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            transform.Translate(new Vector3.left * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            transform.Translate(new Vector3.right * Time.deltaTime * speed);
+        }
     }
 }
